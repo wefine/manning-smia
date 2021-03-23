504621cc956b50feb1fa8bdc0ea2975fa9f0b413
wefine <wefine@aliyun.com>
2021-03-23 15:06:59 +0800

    change to java8

--- chapter4/licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
+++ chapter4/licensing-service/src/main/java/com/optimagrowth/license/service/LicenseService.java
@@ -6,52 +6,51 @@ import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
+import org.springframework.util.ObjectUtils;
 
 import com.optimagrowth.license.model.License;
 
 @Service
 public class LicenseService {
-	
-	@Autowired
-	MessageSource messages;
 
-	public License getLicense(String licenseId, String organizationId){
-		License license = new License();
-		license.setId(new Random().nextInt(1000));
-		license.setLicenseId(licenseId);
-		license.setOrganizationId(organizationId);
-		license.setDescription("Software product");
-		license.setProductName("Ostock");
-		license.setLicenseType("full");
+    @Autowired
+    MessageSource messages;
 
-		return license;
-	}
+    public License getLicense(String licenseId, String organizationId) {
+        License license = new License();
+        license.setId(new Random().nextInt(1000));
+        license.setLicenseId(licenseId);
+        license.setOrganizationId(organizationId);
+        license.setDescription("Software product");
+        license.setProductName("Ostock");
+        license.setLicenseType("full");
 
-	public String createLicense(License license, String organizationId, Locale locale){
-		String responseMessage = null;
-		if(!StringUtils.isEmpty(license)) {
-			license.setOrganizationId(organizationId);
-			responseMessage = String.format(messages.getMessage("license.create.message",null,locale), license.toString());
-		}
+        return license;
+    }
 
-		return responseMessage;
-	}
+    public String createLicense(License license, String organizationId, Locale locale) {
+        String responseMessage = null;
+        if (!ObjectUtils.isEmpty(license)) {
+            license.setOrganizationId(organizationId);
+            responseMessage = String.format(messages.getMessage("license.create.message", null, locale), license.toString());
+        }
 
-	public String updateLicense(License license, String organizationId){
-		String responseMessage = null;
-		if(!StringUtils.isEmpty(license)) {
-			license.setOrganizationId(organizationId);
-			responseMessage = String.format(messages.getMessage("license.update.message", null, null), license.toString());
-		}
+        return responseMessage;
+    }
 
-		return responseMessage;
-	}
+    public String updateLicense(License license, String organizationId) {
+        String responseMessage = null;
+        if (!ObjectUtils.isEmpty(license)) {
+            license.setOrganizationId(organizationId);
+            responseMessage = String.format(messages.getMessage("license.update.message", null, null), license.toString());
+        }
 
-	public String deleteLicense(String licenseId, String organizationId){
-		String responseMessage = null;
-		responseMessage = String.format(messages.getMessage("license.delete.message", null, null),licenseId, organizationId);
-		return responseMessage;
+        return responseMessage;
+    }
 
-	}
+    public String deleteLicense(String licenseId, String organizationId) {
+        String responseMessage = null;
+        responseMessage = String.format(messages.getMessage("license.delete.message", null, null), licenseId, organizationId);
+        return responseMessage;
+    }
 }
